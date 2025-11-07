ab615a78d6a93930b3eefeb706e2cc62236e43a1
Markus Löffler <markusloffler@web.de>
2025-11-07 16:42:12 +0100

    Changed package name

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "BubbleView",
+    name: "swift-bubble-view",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "BubbleView",
-            targets: ["BubbleView"]),
+            name: "SwiftBubbleView",
+            targets: ["SwiftBubbleView"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "BubbleView"),
+            name: "SwiftBubbleView"),
 
     ]
 )
